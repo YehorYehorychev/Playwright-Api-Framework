838cbd157867a87c83c1d12fc6852cbd8773f135
Yehor Yehorychev <egor57899@gmail.com>
2025-11-13 18:02:33 -0800

    First GET request API test has been added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect, request } from "@playwright/test";
 
 test("GET request", async ({ request }) => {
-  const res = await request.get("https://conduit-api.bondaracademy.com/api/tags");
-  console.log(await res.json());
+  const tagsResponse = await request.get("https://conduit-api.bondaracademy.com/api/tags");
+  const tagsResponseJson = await tagsResponse.json();
+
+  expect(tagsResponse.status()).toBe(200);
+  console.log(tagsResponseJson);
 });
